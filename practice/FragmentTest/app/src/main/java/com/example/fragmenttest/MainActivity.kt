96263aefa068ff0c328ea78eb50266ec73dbb008
hongyangxiao <hongyangxiao@bytedance.com>
2020-02-04 22:21:44 +0800

    add some test

---
+++ practice/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fragmenttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 初始化碎片为right_fragment
+        replaceFragment(RightFragment())
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            replaceFragment(AnotherRightFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.right_fragment,fragment)
+        // 模拟返回栈
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
